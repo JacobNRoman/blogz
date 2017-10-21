7eccf079210c89bad4c19181de73161824a83d58
Jacob Roman <jnroman7@gmail.com>
2017-10-21 10:43:43 -0500

    added salts to password hashing

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from flask import Flask, request, redirect, render_template, session, flash
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 import hashlib
+import random
+import string
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
@@ -41,11 +43,18 @@ class User(db.Model):
         self.email = email
         self.pw_hash = make_pw_hash(password)
 
-def make_pw_hash(password):
-    return hashlib.sha256(str.encode(password)).hexdigest()
+def make_salt():
+    return "".join([random.choice(string.ascii_letters) for x in range(5)])
+
+def make_pw_hash(password, salt=None):
+    if not salt:
+        salt = make_salt()
+    hash = hashlib.sha256(str.encode(password + salt)).hexdigest()
+    return '{0},{1}'.format(hash, salt)
 
 def check_pw_hash(password, hash):
-    if make_pw_hash(password) == hash:
+    salt = hash.split(',')[1]
+    if make_pw_hash(password, salt) == hash:
         return True
     return False
 
